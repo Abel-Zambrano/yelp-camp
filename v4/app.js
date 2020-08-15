9c1a4cf0af127a055098839a1f88e82e106d39a6
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-08-14 22:11:42 -0700

    update

--- v4/app.js
+++ v4/app.js
@@ -26,31 +26,31 @@ app.set('view engine', 'ejs');
 //         }
 //     });
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.render('landing')
 })
 
 //INDEX route - show all campgrounds
-app.get('/campgrounds', function(req, res) {
+app.get('/campgrounds', (req, res) => {
     //v2 get all campgrounds from db
-    Campground.find({}, function(err, allCampgrounds){
+    Campground.find({}, (err, allCampgrounds) => {
         if(err){
             console.log(err);
         } else {
-            res.render('index', {campgrounds: allCampgrounds});      
+            res.render('campgrounds/index', {campgrounds: allCampgrounds});      
         }
     })
 })
 
 //CREATE - add new campground to DB
-app.post('/campgrounds', function(req, res) {
+app.post('/campgrounds', (req, res) => {
     //get data from form and add to campgrounds array
     const name = req.body.name;
     const picture = req.body.picture;
     const description = req.body.description;
     const newCampground = {name: name, image: picture, description: description};
     //create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
+    Campground.create(newCampground, (err, newlyCreated) => {
         if(err){
             console.log(err);
         } else {
@@ -62,23 +62,60 @@ app.post('/campgrounds', function(req, res) {
 
 //NEW - show form to create camground
 //sumbits a post request to '/campgrounds'
-app.get('/campgrounds/new', function(req, res) {
-    res.render('new')
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
 })
 
 //SHOW - Shows info on campground
-app.get('/campgrounds/:id', function(req, res){
+app.get('/campgrounds/:id', (req, res) => {
     //find the campground with id
-    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
+    Campground.findById(req.params.id).populate('comments').exec((err, foundCampground) => {
         if(err){
             console.log(err);
             
         } else {
-            res.render('show', {campground: foundCampground});
+            res.render('campgrounds/show', {campground: foundCampground});
+        }
+    });
+});
+//================================================
+//COMMENTS ROUTE
+app.get('/campgrounds/:id/comments/new', (req, res) =>{
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+            
+        } else {
+            res.render('comments/new', {campground: campground});
         }
     });
 });
 
-app.listen(3000, function(){
+app.post('/campgrounds/:id/comments', (req, res) => {
+    //looup campground by id
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+            
+        } else {
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err){
+                    console.log(err);
+                    
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            })
+        }
+    })
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground show page
+});
+
+app.listen(3000, () => {
     console.log('Server is running');
 })
